c233ab07bcb4563763f03c9a1027fba944a402ab
Fernando <fernandoventurini9@hotmail.com>
2023-02-01 20:40:53 +0000

    class react.js 05

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import React from "react"
 
-/* Importando as imagens */
-import Logo from './components/img/Avatar.png'
+/* Importando Components */
+import Header from './components/Header'
+import Body from './components/Body'
 
 export default function App() {
 
-  const canal = () => {
-    return('CFB Cursos')
-  }
-
-  function curso() {
-    return 'Curso de React!'
-  }
-
-  const aluno = () => {
-    return ('Fernando!')
-  }
-
-  //const canal = 'Fazendo o curso de Reactjs na CFB Cursos'
-  //const curso = 'Curso de React.js'
   return(
     <>
-    <header>
-      <h1>{canal()}</h1>
-      <h2>{curso()}</h2>
-    </header>
-
-    <section>
-      <p>{'Aluno: ' + aluno()}</p>
-      <img src={Logo} alt="Logo" />
-      <img src="/img/3.ico" alt="" />
-    </section>
+      <Header/>
+      <Body/>
     </>
   )
+
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function() {
+
+    return(
+        <section>
+            <h2>Curso de React</h2>
+            <p>Se inscreva em nosso canal.</p>
+            <p>Ative o sininho</p>
+        </section>
+    )
+}

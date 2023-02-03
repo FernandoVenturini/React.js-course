73c45b500b4efbbfec3785ea57566b7b6639eb17
Fernando <fernandoventurini9@hotmail.com>
2023-02-03 19:05:46 +0000

    Atualizacao de components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react"
 import './App.css'
 
 /* Importando Components */ 
+import Watch from "./components/Watch"
 
 export default function App() {
 	const css = {
@@ -14,6 +15,9 @@ export default function App() {
 	return(
 		<>
 			<section className="caixa">
+
+			<Watch/>
+
 				<h1 style={{color: '#f00', fontSize: '3em'}}>CFB Cursos</h1>
 				<h2 style={css}>Curso de React</h2>
 				<p className="texto">Se inscreva no canal</p>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function tick() {
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+}
+
+setInterval(tick, 1000);
+
 reportWebVitals();

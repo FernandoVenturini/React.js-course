be19c4c3cfb40d5d6f707504c63a169446b6dda8
Fernando <fernandoventurini9@hotmail.com>
2023-02-03 18:48:17 +0000

    css no react.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react"
 
+/* Importando CSS */
+import './App.css'
+
 /* Importando Components */ 
 
 export default function App() {
@@ -10,8 +13,12 @@ export default function App() {
 
 	return(
 		<>
-			<h1 style={{color: '#f00', fontSize: '3em'}}>CFB Cursos</h1>
-			<h2 style={css}>Curso de React</h2>
+			<section className="caixa">
+				<h1 style={{color: '#f00', fontSize: '3em'}}>CFB Cursos</h1>
+				<h2 style={css}>Curso de React</h2>
+				<p className="texto">Se inscreva no canal</p>
+				<a href="https://lvfcode.com">&lt;LVF_Code&gt;</a>
+			</section>
 		</>
 	)
 }
